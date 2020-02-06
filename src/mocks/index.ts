3bdde1e4255235ce3622549f685875dc8c38ff90
whefter <wh@elunic.com>
2020-02-06 14:09:13 +0100

    fix(mocks): nestjs not included in mocks index to reduce dependency

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,5 +1,4 @@
 export { mockBottlejsLogService } from './bottlejs';
 export { mockAwilixLogService } from './awilix';
-export { MockNestjsLoggerModule } from './nestjs';
 export { MockLogService } from './service';
 export { MockLogger, MockRootLogger } from './logger';
